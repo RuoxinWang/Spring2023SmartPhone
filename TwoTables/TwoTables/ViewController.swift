07610127ed39af3e4570b6cc0b6094b2d1ab3845
RuoxinWang <rwang0104@gmail.com>
2023-02-15 17:49:21 -0800

    Two Tables

--- TwoTables/TwoTables/ViewController.swift
+++ TwoTables/TwoTables/ViewController.swift
@@ -42,8 +42,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         
     }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
-    }
+
 }
 
